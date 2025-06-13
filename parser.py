1311569a6cc963bdd1da85d8712c3f5e387b5fce
Cupid-0x80h <ccuppid09@gmail.com>
2025-06-13 23:46:08 +0545

    remove the init __main__ function it was causing conflicts

--- parser.py
+++ parser.py
@@ -1,166 +1,103 @@
-# =================================================================
-# PART 1: AST NODE CLASSES (The "Bricks")
-# We define the structure of our AST nodes first.
-# =================================================================
+import ply.yacc as yacc
+from lexer import tokens, lexer
 
+# --- AST Node Classes ---
 class AssignmentNode:
-    """Represents assigning a value to a variable. e.g., x = 5;"""
     def __init__(self, name, value, line):
-        self.name = name  # The variable being assigned to (a VariableNode)
-        self.value = value # The value being assigned (e.g., a ConstantNode)
-        self.line = line #and store it 
-
+        self.name = name; self.value = value; self.line = line
     def __repr__(self):
-        # The __repr__ method gives us a nice string representation for printing.
-        # Add the line number to our printout for good measure
         return f"Assignment(line:{self.line}, name={self.name}, value={self.value})"
 
-class FunctionCallNode:
-    """Represents a function call, e.g., gets("input");"""
-    def __init__(self, name, args, line):
-        self.name = name
-        self.args = args
-        self.line = line
+class DeclarationNode:
+    def __init__(self, var_type, name, size=None, line=0):
+        self.var_type = var_type; self.name = name; self.size = size; self.line = line
+    def __repr__(self):
+        if self.size: return f"Declaration(line:{self.line}, type:{self.var_type}, name:{self.name}, size:{self.size})"
+        return f"Declaration(line:{self.line}, type:{self.var_type}, name={self.name})"
 
+class FunctionCallNode:
+    def __init__(self, name, args, line):
+        self.name = name; self.args = args; self.line = line
     def __repr__(self):
         return f"FunctionCall(line:{self.line}, name={self.name.name}, args={self.args})"
 
 class ConstantNode:
-    """Represents a constant value, like a number."""
-    def __init__(self, value):
-        self.value = value
-    
-    def __repr__(self):
-        return f"Constant({self.value})"
+    def __init__(self, value): self.value = value
+    def __repr__(self): return f"Constant({self.value})"
 
 class VariableNode:
-    """Represents a variable name."""
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name): self.name = name
+    def __repr__(self): return f"Variable({self.name})"
 
-    def __repr__(self):
-        return f"Variable({self.name})"
-
-
-# =================================================================
-# PART 2: THE PARSER
-# Now we build the parser that uses the bricks above.
-# =================================================================
-
-import ply.yacc as yacc
-
-# Get the list of token names from our lexer. This is required by PLY.
-from lexer import tokens, lexer
-
-# --- The Grammar Rules ---
-
-# The starting rule. A program is a list of statements.
+# --- Grammar Rules ---
 def p_program_statements(p):
     'program : statement_list'
     p[0] = p[1]
 
 def p_statement_list(p):
-    '''statement_list : statement
-                      | statement_list statement'''
-    if len(p) == 2:
-        p[0] = [p[1]]  # A list containing the first single statement
+    'statement_list : statement_list statement'
+    if p[2]: p[0] = p[1] + (p[2] if isinstance(p[2], list) else [p[2]])
+    else: p[0] = p[1]
+
+def p_statement_list_single(p):
+    'statement_list : statement'
+    p[0] = p[1] if isinstance(p[1], list) else [p[1]]
+
+def p_statement(p):
+    '''statement : assignment_statement
+                 | declaration_statement
+                 | function_call_statement
+                 | return_statement'''
+    p[0] = p[1]
+
+def p_declaration_statement(p):
+    '''declaration_statement : type_specifier IDENTIFIER SEMICOLON
+                             | type_specifier IDENTIFIER LBRACKET NUMBER RBRACKET SEMICOLON
+                             | type_specifier IDENTIFIER ASSIGN expression SEMICOLON'''
+    var_type = p[1]
+    name_node = VariableNode(p[2])
+    line = p.lineno(2)
+    if len(p) == 4:
+        p[0] = DeclarationNode(var_type, name_node, line=line)
+    elif len(p) == 7:
+        p[0] = DeclarationNode(var_type, name_node, size=ConstantNode(p[4]), line=line)
     else:
-        # p[1] is the existing list of statements
-        # p[2] is the new statement to add
-        # We need to handle the case where a statement is None (like our return)
-        if p[2] is not None:
-             p[0] = p[1] + [p[2]]
-        else:
-             p[0] = p[1]
+        p[0] = [DeclarationNode(var_type, name_node, line=line), AssignmentNode(name_node, p[4], line=line)]
 
+def p_type_specifier(p):
+    '''type_specifier : INT_KEYWORD
+                      | CHAR_KEYWORD'''
+    p[0] = p[1]
 
-def p_statement_function_call(p):
-    'statement : IDENTIFIER LPAREN expression RPAREN SEMICOLON'
-    # For now, we are only handling calls with one argument, like gets("...")
-    p[0] = FunctionCallNode(name=VariableNode(p[1]), args=[p[3]], line=p.lineno(1))
+def p_assignment_statement(p):
+    'assignment_statement : IDENTIFIER ASSIGN expression SEMICOLON'
+    p[0] = AssignmentNode(name=VariableNode(p[1]), value=p[3], line=p.lineno(1))
 
-# We also need to teach 'expression' to be a STRING
-def p_expression_string(p):
-    'expression : STRING'
-    p[0] = ConstantNode(p[1])
+def p_function_call_statement(p):
+    'function_call_statement : IDENTIFIER LPAREN expression_list RPAREN SEMICOLON'
+    p[0] = FunctionCallNode(name=VariableNode(p[1]), args=p[3], line=p.lineno(1))
 
+def p_return_statement(p):
+    'return_statement : RETURN expression SEMICOLON'
+    p[0] = None
 
-# Grammar rule for an assignment statement. e.g., score = 100;
-def p_statement_assign(p):
-    '''statement : IDENTIFIER ASSIGN expression SEMICOLON
-                 | INT_KEYWORD IDENTIFIER ASSIGN expression SEMICOLON'''
-    # This logic handles both cases
-    if len(p) == 5:
-        # p.lineno(1) gets the line number of the FIRST token (the identifier)
-        p[0] = AssignmentNode(name=VariableNode(p[1]), value=p[3], line=p.lineno(1))
-    else:
-        # p.lineno(2) gets the line number of the SECOND token (the identifier)
-        p[0] = AssignmentNode(name=VariableNode(p[2]), value=p[4], line=p.lineno(2))
+def p_expression_list(p):
+    '''expression_list : expression
+                       | expression_list COMMA expression'''
+    if len(p) == 2: p[0] = [p[1]]
+    else: p[0] = p[1] + [p[3]]
 
+def p_expression(p):
+    '''expression : IDENTIFIER
+                  | NUMBER
+                  | STRING'''
+    slice_type = p.slice[1].type
+    if slice_type == 'NUMBER': p[0] = ConstantNode(p[1])
+    elif slice_type == 'STRING': p[0] = ConstantNode(p[1])
+    else: p[0] = VariableNode(p[1])
 
-# Grammar rule for a return statement.
-def p_statement_return(p):
-    'statement : RETURN expression SEMICOLON'
-    # For now, we'll just print that we saw a return statement.
-    # In a real compiler, we'd create a ReturnNode.
-    print(f"Parser saw a return statement with value: {p[2]}")
-    p[0] = None # This statement doesn't add a node to our AST for now.
-
-# Grammar rule for what an 'expression' can be. For now, it's just a number.
-def p_expression_number(p):
-    'expression : NUMBER'
-    # Create a ConstantNode for the number.
-    p[0] = ConstantNode(p[1])
-
-# Rule to handle syntax errors.
 def p_error(p):
-    if p:
-        print(f"Syntax error at token: {p.type} ('{p.value}') on line {p.lineno}")
-    else:
-        print("Syntax error at end of input")
+    if p: print(f"Syntax error at token: {p.type} ('{p.value}') on line {p.lineno}")
+    else: print("Syntax error at end of input")
 
-# --- Build the Parser ---
-# This command tells PLY to read all our rules and create the parser.
 parser = yacc.yacc()
-
-
-# =================================================================
-# PART 3: TESTING THE PARSER
-# =================================================================
-
-if __name__ == '__main__':
-    from analyzer import SecurityAnalyzer
-    # Our sample code with a few variables.
-    sample_code = """
-int secret_key = 12345;
-gets("user_input");
-return 0;
-"""
-    # NOTE: Our parser currently ignores the 'int' keyword, but that's okay for our goal.
-    # We are only looking for assignments right now.
-    
-    # Use the parser on our sample code.
-    try:
-        # The parser.parse() function takes the full code string as input.
-        # It will automatically use the lexer behind the scenes.
-        ast = parser.parse(input=sample_code, lexer=lexer)
-        
-        print("\n--- Abstract Syntax Tree ---")
-        if ast:
-            for statement in ast:
-                print(statement)
-            
-        # --- RUN THE SECURITY ANALYSIS ---
-        print("\n--- Security Analysis ---")
-        analyzer = SecurityAnalyzer()
-        warnings = analyzer.analyze(ast)
-
-        if warnings:
-            print("Found security warnings:")
-            for warning in warnings:
-                print(f"  - {warning}")
-        else:
-            print("No security warnings found. Code looks good!")
-            
-    except Exception as e:
-        print(f"An error occurred: {e}")
